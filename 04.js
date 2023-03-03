e8e7ed4a039cd48a39ef761a886408bf9614a1aa
AndreyKomarov <and.kom2017@gmail.com>
2023-03-03 23:28:58 +0300

    Update 04.js

--- 04.js
+++ 04.js
@@ -22,3 +22,15 @@ testStrict(10);
 
 //--------------------------------------------------
 
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
+
+//--------------------------------------------------
+
