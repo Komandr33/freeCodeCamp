646b367a9df6fdd6150f3bf30eed2d6c1158b39a
AndreyKomarov <and.kom2017@gmail.com>
2023-03-13 23:32:17 +0300

    Update 07.js

--- 07.js
+++ 07.js
@@ -41,7 +41,28 @@ switch (val) {
 
 //------------------------------------------
 
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
 
+switch (val) {
+        case "a":
+            answer = 'apple';
+            break;
+        case "b":
+            answer = 'bird';
+            break;
+        case "c":
+            answer = 'cat';
+            break;
+        default: 
+            answer = 'stuff';
+            break;
+    }
+
+  // Only change code above this line
+  return answer;
+}
 
 //------------------------------------------
 
