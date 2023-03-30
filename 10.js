59308c43c9bd70d2ab0ee40c3aa891a70d4135a4
AndreyKomarov <and.kom2017@gmail.com>
2023-03-30 22:53:18 +0300

    Update 10.js

--- 10.js
+++ 10.js
@@ -9,7 +9,17 @@ myDog.bark = 'gav-gav';
 
 //-----------------------------------------------------------------
 
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
 
+// Only change code below this line
+delete myDog.tails;
 
 //-----------------------------------------------------------------
 
