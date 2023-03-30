7af84112e121ea2d7578fd70af80e23c320f17f2
AndreyKomarov <and.kom2017@gmail.com>
2023-03-30 23:12:31 +0300

    Update 10.js

--- 10.js
+++ 10.js
@@ -23,7 +23,25 @@ delete myDog.tails;
 
 //-----------------------------------------------------------------
 
+// Setup
+function phoneticLookup(val) {
+  let result = "";
 
+  // Only change code below this line
+   let lookup = {
+        'alpha': 'Adams',
+        'bravo': 'Boston',
+        'charlie': 'Chicago',
+        'delta': 'Denver',
+        'echo': 'Easy',
+        'foxtrot': 'Frank'
+    }
+  result = lookup[val]
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
 
 //-----------------------------------------------------------------
 
