707bdd3f7de3811bc356f6ceaf7e6d7fa0fc9b70
AndreyKomarov <and.kom2017@gmail.com>
2023-03-31 19:15:48 +0300

    Update 10.js

--- 10.js
+++ 10.js
@@ -101,3 +101,27 @@ const myStorage = {
 const gloveBoxContents = myStorage.car.inside['glove box'];
 
 //-----------------------------------------------------------------
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
+
+//-----------------------------------------------------------------
+
