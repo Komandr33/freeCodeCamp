f7b1873962a35ba3eae39566a3af4cf8dffae494
AndreyKomarov <and.kom2017@gmail.com>
2023-04-02 00:16:32 +0300

    Update 12.js

--- 12.js
+++ 12.js
@@ -58,15 +58,29 @@ convertToInteger("56");
 
 //------------------------------------------------------------------------------
 
+function convertToInteger(str) {
+  return parseInt(str, 2);
+}
 
+convertToInteger("10011");
 
 //------------------------------------------------------------------------------
 
+function checkEqual(a, b) {
+  return a == b ? 'Equal' : 'Not Equal';
+}
 
+checkEqual(1, 2);
 
 //------------------------------------------------------------------------------
 
+function checkSign(num) {
+  return num > 0 ? 'positive'
+  : num < 0 ? 'negative'
+  : 'zero';
+}
 
+checkSign(10);
 
 //------------------------------------------------------------------------------
 
