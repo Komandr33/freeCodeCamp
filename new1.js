e571fe7566fc06dbcfc8ca6c115e20b46bb49c59
AndreyKomarov <and.kom2017@gmail.com>
2023-04-13 22:25:51 +0300

    Update new1.js

--- new1.js
+++ new1.js
@@ -80,11 +80,17 @@ const {today, tomorrow} = HIGH_TEMPERATURES;
 
 //------------------------------------------------------------------
 
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
 
+// Only change code below this line
+  
+const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
 
 //------------------------------------------------------------------
-
-
 
 
 
