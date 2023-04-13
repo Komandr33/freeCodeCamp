79208d0428965f8da2dd4758d260f626cf916478
AndreyKomarov <and.kom2017@gmail.com>
2023-04-14 00:18:32 +0300

    Update new1.js

--- new1.js
+++ new1.js
@@ -92,6 +92,13 @@ const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
 
 //------------------------------------------------------------------
 
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
 
+// Only change code below this line
+const {today: {low: lowToday, high: highToday}} =LOCAL_FORECAST;
 
 //------------------------------------------------------------------
