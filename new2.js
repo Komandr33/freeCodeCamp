9e3a8850743b50f717aa59da918c925fdd5c1dfc
AndreyKomarov <and.kom2017@gmail.com>
2023-04-14 17:38:29 +0300

    Update new2.js

--- new2.js
+++ new2.js
@@ -25,11 +25,43 @@ const half = ({max, min}) => (max + min) / 2.0;
 
 //----------------------------------------------------------------------------
 
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line\
+  'use strict';
+  const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+  // Only change code above this line
 
+  return failureItems;
+}
 
+const failuresList = makeList(result.failure); 
+
+                  // НИЖЕ ТО ЖЕ САМОЕ ДРУГИМ СПОСОБОМ!!!
 //----------------------------------------------------------------------------
 
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line\
+  'use strict';
+  const failureItems = arr.map(i => `<li class="text-warning">${i}</li>`);
+    // Only change code above this line
 
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
 
 //----------------------------------------------------------------------------
 
