b2edb62c9ac336aa8b3e235749960b8ff140e58e
AndreyKomarov <and.kom2017@gmail.com>
2023-04-14 14:34:57 +0300

    Update new1.js

--- new1.js
+++ new1.js
@@ -108,6 +108,14 @@ let a = 8, b = 6;
 
 //------------------------------------------------------------------
 
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [,,...shorterList] = list; // Change this line
+  // Only change code above this line
+  return shorterList;
+}
 
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
 
 //------------------------------------------------------------------
