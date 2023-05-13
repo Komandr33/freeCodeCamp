35379291dc882ae880094fcce72b0b339e08fde4
AndreyKomarov <and.kom2017@gmail.com>
2023-05-13 23:27:14 +0300

    Update 01.js

--- Regular Expressions/01.js
+++ Regular Expressions/01.js
@@ -8,7 +8,9 @@ let result = myRegex.test(myString); // Change this line
 
 //----------------------------------------------------------------------------------
 
-
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
 
 //----------------------------------------------------------------------------------
 
