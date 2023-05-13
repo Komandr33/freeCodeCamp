1959c1b0c1ab12db22e3cb66ef342f2549587a02
AndreyKomarov <and.kom2017@gmail.com>
2023-05-13 23:27:59 +0300

    Update 01.js

--- Regular Expressions/01.js
+++ Regular Expressions/01.js
@@ -4,7 +4,9 @@ let result = myRegex.test(myString); // Change this line
 
 //----------------------------------------------------------------------------------
 
-
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
 
 //----------------------------------------------------------------------------------
 
