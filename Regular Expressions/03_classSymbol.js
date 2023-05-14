ee3d67d416658287897b90a7931b71ff4a20db5d
AndreyKomarov <and.kom2017@gmail.com>
2023-05-14 21:28:57 +0300

    Update 03_classSymbol.js

--- Regular Expressions/03_classSymbol.js
+++ Regular Expressions/03_classSymbol.js
@@ -1,3 +1,9 @@
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi; // в кв. скобках можно указать все варианты возможных совпадений. Например A[nl]a вернёт и 'Anna' и 'Alla'
 let result = quoteSample.match(vowelRegex); 
+
+//----------------------------------------------------------------------------
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // знак "-" указывает на диапозон поиска "от" - "до"
+let result = quoteSample.match(alphabetRegex); 
