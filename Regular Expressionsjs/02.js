06d4b06968561490142a03c5ff24c1cc66eb2604
AndreyKomarov <and.kom2017@gmail.com>
2023-05-14 21:11:05 +0300

    Update 02.js

--- Regular Expressionsjs/02.js
+++ Regular Expressionsjs/02.js
@@ -1,20 +1,4 @@
-let myString = "freeCodeCamp";
-let fccRegex = /freecodecamp/i; //  флаг "i" убирает восприимчивость к регитру
-let result = fccRegex.test(myString);
 
-//----------------------------------------------------------------------------------
-
-let twinkleStar = "Twinkle, twinkle, little star";
-let starRegex = /twinkle/gi;  //  флаг "g" означает - глобальный, т.е. ищет все совпадения в строке, а не первое. Флаг "i" убирает восприимчивость к регитру
-let result = twinkleStar.match(starRegex);
-
-//----------------------------------------------------------------------------------
-
-let exampleStr = "Let's have fun with regular expressions!";
-let unRegex = /.un/; // "." это подстановочный знак, который заменяет любой символ в регулярном выражении
-let result = unRegex.test(exampleStr); 
-
-//----------------------------------------------------------------------------------
 
 
 
